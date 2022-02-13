ebfea54bc2c91b99cbccd4dd678eb46210237fd9
a.o <drealayodeji@icloud.com>
2022-02-13 01:26:58 +0000

    Updated with Gitro

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -52,8 +52,8 @@
 </center>
 
 <footer class="flex flex-row-reverse justify-between lg:mt-16 mt-10 mx-3 items-center">
-    <a href="https://twitter.com/ranch_web" target="_blank" rel="noreferrer"><i class="fab fa-twitter bg-sky-700 text-sky-400 hover:bg-sky-400 hover:text-sky-700 px-2 py-2 rounded-lg"></i></a>
-    <span class="text-sm my-4 lg:my-0">Developed by <a href="https://twitter.com/_realao" target="_blank" rel="noreferrer" class="text-sky-600 underline">Ayodeji</a></span>
+    <a href="https://twitter.com/ranch_web" target="_blank" rel="noreferrer"><i class="fab fa-twitter bg-sky-700 text-sky-400 hover:bg-sky-400 hover:text-sky-700 px-2 py-2 rounded-lg text-lg"></i></a>
+    <span class="text-xs my-4 lg:my-0">Developed by <a href="https://twitter.com/_realao" target="_blank" rel="noreferrer" class="text-sky-600 underline">Ayodeji</a></span>
     
 </footer>
 
