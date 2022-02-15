546f5c15085193600fcbf83061f67dd31f1ba6bb
a.o <drealayodeji@icloud.com>
2022-02-15 16:57:14 +0000

    Updated with Gitro

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Oops! Wrong page</title>
+    <title>Server Error</title>
 
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
