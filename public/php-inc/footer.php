33e4f743a3c70d40e106368613833508accde9eb
a.o <drealayodeji@icloud.com>
2022-02-24 16:14:02 +0000

    Updated with Gitro

--- public/php-inc/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="text-secondary mt-4 mb-5 text-center" style="font-size: 70%;">
-<?php
-  date_default_timezone_set("Europe/London");
-  $yr = date("Y");
-  echo '&copy;'.$yr.' RanCh. Developed By <a href="https://twitter.com/TechAcumenYT" class="link">Ayodeji</a>';
-?>
-</footer>
--- resources/views/chat/index.blade.php
+++ resources/views/chat/index.blade.php
@@ -3,6 +3,18 @@
 @section('content')
 
 <title>Chats</title>
+<style>
+body {
+    background-color: none !important;
+    background-image: url("https://i.ibb.co/JxRPfPZ/ranchbg.jpg");
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-blend-mode: screen;
+}
+.navFixed {
+    background-color: #131313; 
+}
+</style>
 
 @if ($chats->count() == 0)
 
@@ -13,8 +25,9 @@
     <h1 class="text-zinc-100 text-5xl xl:text-6xl font-semibold text-left px-7 mt-10">Chats</h1>
     <div class="mt-6 w-full px-4">
     @foreach ($chats as $chat)
-        <div class="text-zinc-200 bg-zinc-800 w-full p-4 mt-2 rounded-lg">
+        <div class="text-zinc-200 bg-zinc-900 w-full p-4 mt-2 rounded-lg flex justify-between">
         <a href="/chats/{{ strtolower($chat->user1) === strtolower($currentUser) ? $chat->user2 : $chat->user1 }}" class="text-lg font-medium">{{ $chat->user1 == $currentUser ? $chat->user2 : $chat->user1 }}</a>
+        <div class="inline bg-zinc-700 p-[0.15rem] px-[0.5rem] rounded-md font-semibold">{{ $chat->unread_count }}</div>
         </div>
     @endforeach
     </div>
