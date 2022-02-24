f030a9ff348dcaa2acae97a1c7ffce85aef4b604
a.o <drealayodeji@icloud.com>
2022-02-24 16:21:02 +0000

    Updated with Gitro

--- resources/views/chat/index.blade.php
+++ resources/views/chat/index.blade.php
@@ -12,7 +12,7 @@ body {
     background-blend-mode: screen;
 }
 .navFixed {
-    background-color: #141414; 
+    background-color: #181818; 
 }
 </style>
 
@@ -25,8 +25,8 @@ body {
     <h1 class="text-zinc-100 text-5xl xl:text-6xl font-semibold text-left px-7 mt-10">Chats</h1>
     <div class="mt-6 w-full px-4">
     @foreach ($chats as $chat)
-        <div class="text-zinc-200 bg-zinc-900 w-full p-4 mt-2 rounded-lg flex justify-between">
-        <a href="/chats/{{ strtolower($chat->user1) === strtolower($currentUser) ? $chat->user2 : $chat->user1 }}" class="text-lg font-medium">{{ $chat->user1 == $currentUser ? $chat->user2 : $chat->user1 }}</a>
+        <div class="text-zinc-200 bg-zinc-900 w-full p-4 mt-2 px-7 rounded-lg flex justify-between">
+        <a href="/chats/{{ strtolower($chat->user1) === strtolower($currentUser) ? $chat->user2 : $chat->user1 }}" class="text-lg font-medium hover:text-sky-500">{{ $chat->user1 == $currentUser ? $chat->user2 : $chat->user1 }}</a>
         <div class="inline bg-zinc-700 p-[0.15rem] px-[0.5rem] rounded-md font-semibold {{ $chat->unread_count == 0 ? 'text-white' : 'text-red-500' }}">
             {{ $chat->unread_count }}
         </div>
