b98741852ac1749ae4688f15ba0a6e03e5a31071
a.o <drealayodeji@icloud.com>
2022-02-24 15:33:16 +0000

    Updated with Gitro

--- resources/views/chat/view.blade.php
+++ resources/views/chat/view.blade.php
@@ -22,7 +22,7 @@
                     <div class="flex w-full justify-end">
                         <!-- Sent by user -->
                         <div
-                            class="bg-sky-700 text-white w-auto max-w-[75vw] xl:max-w-[55%] py-2 px-5 rounded-2xl font-medium break-words mb-4 bubble">
+                            class="text-white w-auto max-w-[75vw] xl:max-w-[55%] py-2 px-5 rounded-2xl font-medium break-words mb-4 bubble">
 
                             {{ $chat->content }}
 
