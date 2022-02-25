e3a9d4f8baa6fd462d92cfff619192081a667445
a.o <drealayodeji@icloud.com>
2022-02-25 22:41:58 +0000

    Updated with Gitro

--- public/js/chat.js
+++ public/js/chat.js
@@ -179,22 +179,3 @@ white.addEventListener("click", () => {
     white.classList.add("py-2");
     localStorage.setItem("bubble_color", "white");
 });
-
-
-
-//Continually send AJAX calls to update user's status
-
-setInterval(() => {
-    const xhr = new XMLHttpRequest();
-    const chatUser = document.getElementById("chat_user").innerText
-    const lastSeen = document.getElementById("last_seen")
-
-    xhr.open("GET", `/online?user=${chatUser}`, true);
-
-    xhr.onload = () => {
-        lastSeen.innerText = xhr.responseText;
-    }
-
-    xhr.send();
-
-}, 12000)
