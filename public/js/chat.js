dba5d5b0332a422db894ce69aaecd0d28ff255fa
a.o <drealayodeji@icloud.com>
2022-02-26 02:59:16 +0000

    Updated with Gitro

--- public/js/chat.js
+++ public/js/chat.js
@@ -114,6 +114,31 @@ window.onload = () => {
     const SelectedBubbleColor = document.getElementById(currentBubbleColor);
 
     SelectedBubbleColor.classList.add("py-2"); //Indicate the current bubble color
+    
+//This block gets the user status before interval starts
+    const xhr = new XMLHttpRequest();
+    const chatUser = document.getElementById("chat_user").innerText
+    const lastSeen = document.getElementById("last_seen")
+
+    xhr.open("GET", `/online?user=${chatUser}`, true);
+
+    xhr.onprogress = () => {
+        lastSeen.innerText = "Loading...";
+    }
+
+    xhr.onloadstart = () => {
+        lastSeen.innerText = "Loading...";
+    }
+
+    xhr.onload = () => {
+        lastSeen.innerText = xhr.responseText;
+    }
+
+    xhr.onerror = () => {
+        lastSeen.innerText = "Something went wrong...";
+    }
+
+    xhr.send();
 };
 
 //Red bubble
