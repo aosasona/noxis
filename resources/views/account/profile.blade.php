c1cc4ad7da2aae27d1fee257e40f1e494af156d5
a.o <drealayodeji@icloud.com>
2022-02-26 01:05:41 +0000

    Updated with Gitro

--- resources/views/account/profile.blade.php
+++ resources/views/account/profile.blade.php
@@ -16,7 +16,7 @@
             </div>
 
             @if (strtolower(Cookie::get('username')) === strtolower($user[0]->username))
-            <div class='flex flex-row w-2/3 h-auto items-center justify-center bg-zinc-800 lg:w-1/3 py-2'>
+            <div class='flex flex-row w-[90%] h-auto items-center justify-center bg-zinc-800 lg:w-1/3 py-2'>
 
                 <input value="https://noxis.app/chats/{{ Cookie::get('username') }}"
                     class='py-3 px-3 w-4/5 text-left text-white bg-transparent  font-medium'
