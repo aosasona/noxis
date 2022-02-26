05be5d8756c130fb284dc2a5209ae905593688db
a.o <drealayodeji@icloud.com>
2022-02-26 13:54:06 +0000

    Updated with Gitro

--- resources/views/chat/index.blade.php
+++ resources/views/chat/index.blade.php
@@ -24,7 +24,7 @@ body {
 <div class="mx-auto w-screen h-screen xl:w-3/5 py-4 text-left">
     <h1 class="text-zinc-100 text-5xl xl:text-6xl font-semibold text-left px-7 mt-10">Chats</h1>
     <div class="mt-6 w-full px-4">
-        <div class="text-xs text-zinc-200">Tip: Refresh in-chat to see new messages</div>
+        <div class="text-xs text-zinc-200 px-1">Tip: Refresh in-chat to see new messages</div>
     @foreach ($chats as $chat)
         <div class="text-zinc-200 bg-zinc-900 w-full p-4 mt-2 px-7 rounded-lg flex justify-between">
         <a href="/chats/{{ strtolower($chat->user1) === strtolower($currentUser) ? $chat->user2 : $chat->user1 }}" class="text-lg font-medium hover:text-sky-500">{{ $chat->user1 == $currentUser ? $chat->user2 : $chat->user1 }}</a>
