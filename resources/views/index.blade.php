e6c6848a4dfd9e21c8a6ede15a4cf6c96a4dafb6
a.o <drealayodeji@icloud.com>
2022-02-28 19:45:20 +0000

    Updated with Gitro

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<title>Noxis - Fun, Easy & Anonymous Conversations</title>
+<title>Noxis - Quick, Fun, Easy & Anonymous Conversations</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/3.1.2/fullpage.min.css" integrity="sha512-4rPgyv5iG0PZw8E+oRdfN/Gq+yilzt9rQ8Yci2jJ15rAyBmF0HBE4wFjBkoB72cxBeg63uobaj1UcNt/scV93w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 
@@ -12,16 +12,27 @@
                     <div class="box3 w-4/5">
                         <div class="text-center">
                             <h1 class="text-white text-sm xl:text-3xl font-semibold inline mainText">Say hi to <span class="text-sky-500 typedText"></span> chats.</h1>
-                        <div class="flex flex-col items-center xl:flex-row w-full xl:justify-evenly font-medium mt-7 xl:mt-10 text-xs xl:text-lg">
-                        <a href="/signup" class="p-2 px-5 bg-sky-700 text-white rounded-md w-4/5 xl:w-1/3 mx-2 my-2 xl:my-0">Sign Up</a>
-                        <a href="/signin" class="p-2 px-5 bg-sky-700 text-white rounded-md w-4/5 xl:w-1/3 mx-2 my-2 xl:my-0">Sign In</a>
-                        <a href="/chats" class="p-2 px-5 bg-sky-700 text-white rounded-md w-4/5 xl:w-1/3 mx-2 my-2 xl:my-0"><i class="fa-solid fa-user-secret"></i> Continue as Anon</a>
+                        <div class="flex flex-col items-center xl:flex-row w-full xl:justify-evenly font-medium mt-7 xl:mt-10 text-xs xl:text-lg" id="homeLinks">
+                        <a href="/signup" class="p-2 px-5 bg-sky-700 hover:border-sky-700 text-white rounded-md w-4/5 xl:w-1/3 mx-2 my-2 xl:my-0">Get Started</a>
+                        <a href="/signin" class="p-2 px-5 border-2 border-sky-700 text-white rounded-md w-4/5 xl:w-1/3 mx-2 my-2 xl:my-0">Sign In</a>
+                        <a href="/chats" class="p-2 px-5 bg-zinc-700 text-white rounded-md w-4/5 xl:w-1/3 mx-2 my-2 xl:my-0"><i class="fa-solid fa-user-secret"></i> Continue as Anon</a>
                         </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+
+        <div class="section">
+            <div class="w-full text-left lg:text-center font-light lg:font-medium text-lg lg:text-2xl text-zinc-400">
+                <div class="m-auto w-3/5">
+                Noxis is a web service aimed at giving you a platform to communicate quickly, easily & anonymously with whomever you want to chat with – 
+                without the distractions of setting passwords or filling long, boring forms. We don't have the standard registration process users usually go through, we don’t serve ads either, we serve you 😎
+            </div></div>
+        </div>
+
+        
+
     </div>
 
 
@@ -35,6 +46,10 @@
         css3: true,
 	    scrollingSpeed: 500,
         fixedElements: '.navBar',
+        navigation: true,
+        navigationPosition: 'right',
+        navigationTooltips: ['Next', 'Previous'],
+        scrollBar: true,
     });
     
     //methods
@@ -42,12 +57,12 @@
 
     //TypedJS
     var options = {
-    strings: ['easy', 'anonymous', 'fun'],
+    strings: ['quick', 'easy', 'anonymous', 'fun'],
     typeSpeed: 140,
     loop: true,
     loopCount : Infinity,
     startDelay: 200,
-    backDelay: 1400,
+    backDelay: 1000,
 };
 
 var typed = new Typed('.typedText', options);
