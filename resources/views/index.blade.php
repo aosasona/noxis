b809d6efa14e1d9287a4c37822a9f290d0f34769
a.o <drealayodeji@icloud.com>
2022-02-28 19:10:06 +0000

    Updated with Gitro

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,16 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
+<title>Noxis - Fun, Easy & Anonymous Conversations</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/3.1.2/fullpage.min.css" integrity="sha512-4rPgyv5iG0PZw8E+oRdfN/Gq+yilzt9rQ8Yci2jJ15rAyBmF0HBE4wFjBkoB72cxBeg63uobaj1UcNt/scV93w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 
     <div id="fullpage">
         <div class="section" id="first">
-            <div class="box w-4/5">
+            <div class="box w-5/6">
                 <div class="box2 h-4/5">
-                    <div class="box3 w-4/5 border-none lg:border-2 lg:border-[#454545]">
+                    <div class="box3 w-4/5">
                         <div class="text-center">
-                            <h1 class="text-white text-lg xl:text-3xl font-semibold inline mainText">Say hi to <span class="text-sky-500 typedText"></span> chats.</h1>
+                            <h1 class="text-white text-sm xl:text-3xl font-semibold inline mainText">Say hi to <span class="text-sky-500 typedText"></span> chats.</h1>
+                        <div class="flex flex-col items-center xl:flex-row w-full xl:justify-evenly font-medium mt-7 xl:mt-10 text-xs xl:text-lg">
+                        <a href="/signup" class="p-2 px-5 bg-sky-700 text-white rounded-md w-4/5 xl:w-1/3 mx-2 my-2 xl:my-0">Sign Up</a>
+                        <a href="/signin" class="p-2 px-5 bg-sky-700 text-white rounded-md w-4/5 xl:w-1/3 mx-2 my-2 xl:my-0">Sign In</a>
+                        <a href="/chats" class="p-2 px-5 bg-sky-700 text-white rounded-md w-4/5 xl:w-1/3 mx-2 my-2 xl:my-0"><i class="fa-solid fa-user-secret"></i> Continue as Anon</a>
+                        </div>
                         </div>
                     </div>
                 </div>
