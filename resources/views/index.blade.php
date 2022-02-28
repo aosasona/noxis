cdf0465fb9dfcc23a8b733be26657a010df264e5
a.o <drealayodeji@icloud.com>
2022-02-28 20:54:10 +0000

    Updated with Gitro

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,12 +7,12 @@
 
     <div id="fullpage">
         <div class="section" id="first">
-            <div class="box w-5/6">
-                <div class="box2 h-4/5">
-                    <div class="box3 w-4/5">
+            <div class="box w-[95%] lg:w-5/6">
+                <div class="box2 w-[95%] lg:h-4/5">
+                    <div class="box3 w-[95%] lg:w-4/5 py-3">
                         <div class="text-center">
-                            <img src="{{ asset('img/laptop.png') }}" class="w-3/5 m-auto hidden lg:block" />
-                            <img src="{{ asset('img/mobile.png') }}" class="w-full m-auto lg:hidden" />
+                            <img src="{{ asset('img/laptop.png') }}" class="w-3/5 m-auto hidden lg:block" id="homeImg" />
+                            <img src="{{ asset('img/mobile.png') }}" class="w-full m-auto lg:hidden" id="homeImg" />
                             <h1 class="text-white text-sm lg:text-3xl font-semibold inline mainText">Say hi to <span class="text-sky-500 typedText"></span> chats.</h1>
                         <div class="flex flex-col items-center lg:flex-row w-full lg:justify-evenly font-medium mt-7 lg:mt-10 text-xs lg:text-lg" id="homeLinks">
                         <a href="/signup" class="p-2 px-5 bg-sky-700 hover:border-2 hover:bg-transparent hover:border-sky-700 text-white rounded-md w-4/5 lg:w-1/3 mx-2 my-2 lg:my-0">Get Started</a>
@@ -31,6 +31,10 @@
                 <b class="text-sky-600">Noxis</b> is a web service aimed at giving you a platform to communicate quickly, easily & anonymously with whomever you want to chat with – 
                 without the distractions of setting passwords or filling long, boring forms. We don't have the standard registration process users usually go through, we don’t serve ads either, we serve you 😎
             </div></div>
+            <div class="circle hidden lg:block"></div>
+            <div class="circle_second hidden lg:block"></div>
+            <div class="square block lg:hidden"></div>
+            <div class="square_second block lg:hidden"></div>
         </div>
 
         
